b4c98dcd48962691af69a58d9235f3e5d091171b
Pirmin Kalberer <pka@sourcepole.ch>
2009-07-09 19:20:52 +0200

    Database authentication

--- app/controllers/database_controller.rb
+++ app/controllers/database_controller.rb
@@ -1,6 +1,13 @@
 class DatabaseController < ApplicationController
   #active_scaffold :post
 
+  def logout
+    @realm = 'Login Required'
+    response.headers["Status"] = "Unauthorized"
+    response.headers["WWW-Authenticate"] = "Basic realm=\"#{@realm}\""
+    render :text => '', :status => 401
+  end
+
   def refresh_tables
     @database = Database.find_by_datname(params[:database_name])
     @tables = @database.tables
--- app/models/table.rb
+++ app/models/table.rb
@@ -2,6 +2,10 @@ class Table < ActiveRecord::Base
   set_table_name "posts"
 
   def self.change_table(name)
+#    @ar_class ||= Class.new(ActiveRecord::Base) do
+#       set_table_name name
+#       set_primary_key 'id'
+#    end
     set_table_name name
     reset_column_information
   end
