8150cc30d8f3a885cb654c886178c08d1f3c035e
Pirmin Kalberer <pka@sourcepole.ch>
2009-07-10 18:21:49 +0200

    Databases start view

--- app/controllers/databases_controller.rb
+++ app/controllers/databases_controller.rb
@@ -1,5 +1,9 @@
 class DatabasesController < ApplicationController
-  active_scaffold
+  active_scaffold do |config|
+    config.list.columns = [:datname, :owner, :datacl, :datallowconn, :datconfig, :datconnlimit, :datistemplate]
+    #:dattablespace, :encoding
+    config.actions = [:list]
+  end
 
   def refresh_tables
     @database = Database.find_by_datname(params[:database_name])
--- app/helpers/databases_helper.rb
+++ app/helpers/databases_helper.rb
@@ -1,2 +1,5 @@
 module DatabasesHelper
+  def owner_column(record)
+    h(record.owner.rolname)
+  end
 end
