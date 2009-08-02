0a10f81c647f4ea4940a6115c096f0d9dcaebb0e
Pirmin Kalberer <pka@sourcepole.ch>
2009-08-02 23:16:45 +0200

    DB&Schema dropdown selection in Rows

--- app/controllers/rows_controller.rb
+++ app/controllers/rows_controller.rb
@@ -2,13 +2,23 @@ class RowsController < ApplicationController
   active_scaffold :application #Create actions. Note: called each time in DEV env.
   layout "databases"
 
+  before_filter :set_database
   before_filter :refresh_scaffold
 
   protected
 
+  def set_database
+    @database = Database.find_by_name(params[:database_name])
+    if @database
+      params[:schema_name] ||= 'public'
+      @db_schema = DbSchema.find_by_name(params[:schema_name])
+      @tables = @database.tables #FIXME: @db_schema.tables
+    end
+  end
+
   def refresh_scaffold
     unless @table
-      @database = Database.find_by_name(params[:database_name])
+      #@database = Database.find_by_name(params[:database_name])
       @table = params[:table_name]
       logger.debug "refresh_scaffold to #{@table}"
       table_class = Table.new.connect_table(@database, @table)
